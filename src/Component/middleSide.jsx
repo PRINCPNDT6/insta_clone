7ec2cb67cfff2418cebb5a28a0be73a8c58fe1a2
Prince Sharma <krrishsharma698@gmail.com>
2026-03-24 16:04:15 +0530

    deploay

---
+++ src/Component/middleSide.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import './middleSide.css';
+import ProfileStory from './middleprofileStory.jsx';
+import StoryViewer from './StoryViewer.jsx';
+import MiddlePost from './middlePost.jsx';
+
+import postFirstsiddique from '../assets/postFirstsiddique.mp4';
+import vibhanshuprofile from '../assets/vibhanshuprofile.jpg';
+import hariomshreds from '../assets/hariomshreds.jpg';
+ 
+ import caliathletics from '../assets/caliathletics.jpg';
+import srijiihh from '../assets/srijiihh.jpg';
+import srijiihh1 from '../assets/srijiihh1.mp4';
+import  caliathletics1 from '../assets/caliathletics1.mp4';
+import souriya from '../assets/souriya.jpg'
+import tallwinder from '../assets/tallwinder.jpg'
+import abhishek from '../assets/abhishek.jpg'
+import nikshu from '../assets/nikshu.jpg'
+import calesthenicsgirl from '../assets/calesthenicsgirl.mp4'
+import calesthenicgirl from '../assets/calesthenicgirl.jpg'
+import siddique from '../assets/siddique.jpg'
+
+
+function MiddleSide() {
+  const [activeStory, setActiveStory] = useState(null);
+  const [isStoryOpen, setIsStoryOpen] = useState(false);
+
+  //  Global mute/unmute state (shared across all videos)
+  const [isMuted, setIsMuted] = useState(true);
+
+  // Toggle mute globally
+  const handleGlobalMute = () => {
+    setIsMuted((prev) => !prev);
+  };
+
+  // Story list
+  // video: postFirstsiddique,
+  const stories = [
+    {   img: vibhanshuprofile, profileName: 'vibhanshu...' },
+    {  img: souriya, profileName: 'hariomshr...' },
+    {  img: tallwinder, profileName: 'talwwinder...' },
+    {  img: abhishek, profileName: 'Abhishek...' },
+    {  img: nikshu, profileName: 'nikshu...' },
+    { video: postFirstsiddique,img: hariomshreds, profileName: 'hariomshr...' },
+  ];
+
+  return (
+    <>
+      <div className="middleSideSection">
+        {/*  Story Section */}
+        <div className="storyProfileContainer">
+          {stories.map((story, index) => (
+            <div key={index} onClick={() => setActiveStory(story)}>
+              <ProfileStory img={story.img} profileName={story.profileName} />
+            </div>
+          ))}
+        </div>
+
+        {/*  Post Section */}
+        <div className="postContainer">
+          <div className="middlePost">
+            <MiddlePost
+              profileImg={caliathletics}
+              profileName="caliathletics"
+              date="• 2d"
+              
+              suggestDetails="Gym addict"
+              firstpost={caliathletics1}
+              isMuted={isMuted}
+              onGlobalMute={handleGlobalMute}
+                
+              isStoryOpen={isStoryOpen}
+            />
+            <MiddlePost
+              profileImg={calesthenicgirl}
+              profileName="Calesthenics_girl"
+              date="• 2d"
+              suggestDetails="suggested for you"
+              firstpost={calesthenicsgirl}
+              isMuted={isMuted}
+              onGlobalMute={handleGlobalMute}
+              
+                isStoryOpen={isStoryOpen}
+            />
+             
+            <MiddlePost
+              profileImg={srijiihh}
+              profileName="srijiihh"
+              date="• 2d"
+              suggestDetails="original"
+              firstpost={srijiihh1}
+              isMuted={isMuted}
+               onGlobalMute={handleGlobalMute}
+             
+                isStoryOpen={isStoryOpen}
+            />
+            <MiddlePost
+              profileImg={siddique}
+              profileName="siddique"
+              date="• 2d"
+              suggestDetails="suggested for you"
+              firstpost={postFirstsiddique}
+              isMuted={isMuted}
+              onGlobalMute={handleGlobalMute}
+                
+                isStoryOpen={isStoryOpen}
+            />
+             
+          </div>
+        </div>
+      </div>
+
+      {/*  Story Viewer (popup/modal) */}
+      <StoryViewer story={activeStory} onClose={() => setActiveStory(null)} />
+    </>
+  );
+}
+
+export default MiddleSide;
---
+++ src/Component/rightSide.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import RightMainprofile from './rightMainprofile.jsx'
+ import profile2 from '../assets/profile2.jpg';
+ import './rightSide.css';
+import RightOtherProfile from './rightOtherProfile.jsx';
+import vibhanshuprofile from '../assets/vibhanshuprofile.jpg'
+import devansh from '../assets/devansh.jpg'
+import nikshu from '../assets/nikshu.jpg';
+import vanshpndtt from '../assets/vanshpndtt.jpg';
+import vipulfit from '../assets/vipulfit.jpg';
+import ofcprisha  from '../assets/ofcprisha.jpg'
+import share1 from '../assets/share1.png'
+import hariomshreds from '../assets/hariomshreds.jpg'
+function RightSide() {
+    return (
+         <>
+       <div className="rightSidepage">
+        <div className="rightsidebox">
+             <RightMainprofile  profile= {profile2} userName="swolprinc" Name= "PRISHU" follow= "Switch"/>
+         <div className="suggestnav">
+             
+                <div>Suggested for you</div>
+                <div>See All</div>
+             
+        </div>
+        </div>
+            <div className="otherProfile">
+                <RightOtherProfile ORprofileImg={devansh} UserName= "mr_devanshrajput" FollowedName= "Followed by shubham_Tomar"/>
+                <RightOtherProfile ORprofileImg={nikshu} UserName= "nikshu_pndtt" FollowedName= "Followed by prishu_sharma07"/>
+                <RightOtherProfile ORprofileImg={vanshpndtt} UserName= "vansh_pandat_0007" FollowedName= "Followed by jayant_dada"/>
+                <RightOtherProfile ORprofileImg={ofcprisha} UserName= "Ofc_Prisha" FollowedName= "Followed by ritika"/>
+                <RightOtherProfile ORprofileImg={vipulfit} UserName= "vip_ulfit" FollowedName= "Followed by pndtt_vansh002"/>
+                 
+            </div>
+         
+         <div className="fixedprofile">
+                   <div className="box">
+                       <div className='imgMessage'>
+                              <img  className = "share" src={share1} alt="Share" /> 
+                         
+                       </div>
+                       <div className="borderimg">
+                         <div> <span>Messages</span></div>
+                       <div className="rightnav">
+                         <div className='frstimg'> <img   src={vibhanshuprofile} alt="" /></div>
+                        <div className='secndimg'><img   src={nikshu} alt="" /></div>
+                        <div className='thrdimg'><img   src={hariomshreds} alt="" /></div>
+                       <div className="threedot">
+                           <div className='first dot'></div>
+                           <div className='second dot'></div>
+                           <div className='third dot'></div>
+                       </div>
+                       </div>
+                       </div>
+                   </div>
+                </div>
+         
+        </div>
+         </>
+        
+        
+       
+    )
+}
+
+export default RightSide 
